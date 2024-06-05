7363579bf954c8c32936e43a9e5b896b4e5fb9cf
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-06-05 17:16:50 -0400

    first

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+const passportJWT = require('passport-jwt');
+const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+//const swaggerDoc = require('./docs/swagger.json');
+const swaggerJsdoc = require('swagger-jsdoc');
+const path = require('path');
 
 // create express app & set all content to json
 const app = express();
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // db conn
 if (process.env.NODE_ENV !== 'production') {
@@ -16,15 +26,82 @@ mongoose.connect(process.env.CONNECTION_STRING, {})
 .catch((err) => { console.log (`DB Connection Failed ${err}`); });
 
 // enable CORS for angular client app BEFORE controllers
-const cors = require('cors');
 app.use(cors({
     origin: process.env.CLIENT_URL,
-    methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS'
+    methods: 'GET,POST,PUT,DELETE,HEAD,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization'
 }));
+
+// passport config BEFORE routers
+app.use(session({
+    secret: process.env.PASSPORT_SECRET,
+    resave: true,
+    saveUninitialized: false
+  }));
+  
+app.use(passport.initialize());
+app.use(passport.session());
+
+// link passport to User model
+const User = require('./models/user');
+passport.use(User.createStrategy());
+
+// link User model w/passport session mgmt
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// setup JWT Options
+let JWTStrategy = passportJWT.Strategy;
+let ExtractJWT = passportJWT.ExtractJwt;
+
+let jwtOptions = 
+{
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.PASSPORT_SECRET
+}
+
+// setup JWT Strategy
+let strategy = new JWTStrategy(jwtOptions, function(jwt_payload, done)
+{
+    try 
+    {
+        const user = User.findById(jwt_payload.id);
+        if (user) 
+        {
+            return done(null, user);
+        }
+        return done(null, false);
+    } 
+    catch (error) 
+    {
+        return done(error, false);
+    }
+});
+
+passport.use(strategy);
 
 // map routes
 const postsController = require('./controllers/posts');
+const usersController = require('./controllers/users');
 app.use('/v1/api/posts', postsController);
+app.use('/v1/api/users', usersController);
+
+const options = {
+    swaggerDefinition: {
+        info: {
+            title: 'Blog API',
+            version: '1.0.0',
+            description: 'In-Class Express API'
+        }
+    },
+    apis: [path.join(__dirname, '/controllers/*.js')]
+};
+const swaggerSpecs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+app.use(express.static(__dirname + '/public'));
+app.get('*', (req, res) => { res.sendFile(__dirname + '/public/index.html')});
 
 // start server
 app.listen(3000);
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+const passport = require('passport');
+const users = require('./users');
 
+const jwt = require('jsonwebtoken');
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+/**
+ * @swagger
+ * /v1/api/posts:
+ *  get:
+ *      description: Returns all blog posts
+ *      responses:
+ *          200:
+ *              description: OK 
+ *          400:
+ *              description: Bad Request
+ */
 /* GET: /api/posts => show all posts */
 router.get('/', async (req, res, next) => {
     try {
@@ -15,8 +33,59 @@ router.get('/', async (req, res, next) => {
     }        
 });
 
+/**
+ * @swagger
+ * /v1/api/posts:
+ *  post:
+ *      description: Create new blog post
+ *      consumes:
+ *          - application/json:
+ *      parameters:
+ *          - in: body
+ *            name: post
+ *            schema:
+ *              type: object
+ *              required:
+ *                  - title
+ *                  - body
+ *                  - username
+ *              properties:
+ *                  title:
+ *                    type: string
+ *                  body:
+ *                      type: string
+ *                  username:
+ *                      type: string
+ *      responses:
+ *          201:
+ *              description: Created
+ *          400:
+ *              description: Bad Request
+ *          401:
+ *              description: Unauthorized
+ */
 /* POST: /api/posts => create new post from http request body */
 router.post('/', async (req, res, next) => {
+
+    try {
+    //console.log(req);
+        const token = req.cookies.authToken;
+        if (token) {
+            //console.log(token);
+            const decode = jwt.verify(token, process.env.PASSPORT_SECRET);
+            const post = await Post.create(req.body);
+            return res.json(post).status(201); // 201: Resource Created
+        }
+        else {
+            console.log('no token');
+            return res.status(401).json({msg: 'Unauthorized'});
+        }
+    }
+    catch (err) {
+        return res.json(err).status(400); // 400: Bad Request
+    }
+
+/*
     try {
         const post = await Post.create(req.body);
         return res.json(post).status(201); // 201: Resource Created
@@ -24,13 +93,48 @@ router.post('/', async (req, res, next) => {
     catch(err) {
         return res.json(err).status(400); // 400: Bad Request
     }
+    */
 });
 
 /* DELETE: /api/posts/abc123 => delete selected post */
 router.delete('/:_id', async (req, res, next) => {
     try {
-        await Post.findByIdAndDelete(req.params._id);
-        return res.json({}).status(204); // 204: No Content
+        const token = req.cookies.authToken;
+        if (token) {
+            //console.log(token);
+            const decode = jwt.verify(token, process.env.PASSPORT_SECRET);
+            console.log(decode);
+        // console.log('delete start');
+        // const username = users.verifyToken(req);
+        // console.log('un: ' + username);
+        // if (username == 'undefined') {
+        //     return res.status(401).json({msg: 'Unauthorized'});
+        //}
+        //else {
+            let post = await Post.findById(req.params._id);
+
+            console.log(post);
+            if (post == null) {
+                console.log('404');
+                return res.status(404).json({msg: 'Post Not Found'});
+            }
+            else {
+                //console.log(`req.user: ${req.user}`);
+                if (post.username !== decode.username) {
+                    console.log('403');
+                    return res.status(403).json({msg: 'Unauthorized'});
+                }
+                else {
+                    await Post.findByIdAndDelete(req.params._id);
+                    console.log('204');
+                    return res.status(204).json({}); // 204: No Content
+                }
+            }
+        }
+        else {
+            console.log('no token');
+            return res.status(401).json({msg: 'Unauthorized'});
+        }
     }
     catch(err) {
         return res.json(err).status(404); // 404: Not Found
@@ -40,11 +144,32 @@ router.delete('/:_id', async (req, res, next) => {
 /* PUT: /api/posts/abc123 => update selected post */
 router.put('/:_id', async (req, res, next) => {
     try {
-        let post = await Post.findByIdAndUpdate(req.params._id, req.body);
-        return res.json(post).status(202); // 202: Resource Modified
+        //console.log(req);
+            const token = req.cookies.authToken;
+            if (token) {
+                //console.log(token);
+                const decode = jwt.verify(token, process.env.PASSPORT_SECRET);
+
+                //let post = await Post.findByIdAndUpdate(req.params._id, req.body);
+                let post = await Post.findById(req.params._id);
+
+                if (!post) {
+                    return res.status(404).json({msg: 'Post Not Found'});
+                }
+
+                if (post.username != decode.username) {
+                    return res.status(403).json({msg: 'Unauthorized'});
+                }
+
+                await Post.findByIdAndUpdate(req.params._id, req.body);
+                return res.status(202).json(post); // 202: Resource Modified
+            }
+            else {
+                return res.status(401).json({msg: 'Unauthorized'}); // 401: Unauthorized
+            }
     }
     catch(err) {
-        return res.json(err).status(404); // 404: Not Found
+        return res.status(404).json(err); // 404: Not Found
     }
 });
 
